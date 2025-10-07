ab239d770d0b68ceba49e2a62f4e1911ec34ba4d
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-06 22:58:48 -0500

    Update HospitalSystem.java
    User interactively enters details and exceptions are added if the age and phonenumber are incorrectly entered it throws an exception saying age must be between 0 and 100, invalid phone number
    making sure all the exceptions are executed

--- HospitalSystem.java
+++ HospitalSystem.java
@@ -1,30 +1,48 @@
+import java.util.Scanner;
+
 class Patient {
     private String name;
     private int age;
     private String disease;
-    private String contactNumber;
+    private String phonenumber;
 
-    public Patient(String name, int age, String disease, String contactNumber) {
-        if (age <= 0) throw new IllegalArgumentException("Age must be positive");
-        if (!contactNumber.matches("\\d{10}")) throw new IllegalArgumentException("Invalid contact number");
+    public Patient(String name,int age,String disease,String phonenumber) {
+        String errorMessage = "";
+        if (age < 0 || age > 100)
+            errorMessage += "Age must be between 0 and 100. ";
+        if (!phonenumber.matches("\\d{10}"))
+            errorMessage += "Invalid phone number. ";
+
+        if (!errorMessage.isEmpty())
+            throw new IllegalArgumentException(errorMessage);
         this.name = name;
         this.age = age;
-        this.disease = disease;
-        this.contactNumber = contactNumber;
+        this.disease=disease;
+        this.phonenumber=phonenumber;
     }
-
     public String getPatientSummary() {
-        return "Patient: " + name + ", Age: " + age + ", Disease: " + disease;
+        return "Patient: " + name + " Age: " + age + " Disease: " + disease;
     }
 }
-
 public class HospitalSystem {
     public static void main(String[] args) {
-        try {
-            Patient p1 = new Patient("Vinny", 28, "Flu", "9876543210");
-            System.out.println(p1.getPatientSummary());
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+        Scanner sc = new Scanner(System.in);
+        try{
+           System.out.println("Please enter Patient name: ");
+           String name = sc.nextLine();
+           System.out.println("Please enter Patient age: ");
+           int age = sc.nextInt();
+           sc.nextLine();
+           System.out.println("Please enter DISEASE Name: ");
+           String disease=sc.nextLine();
+           System.out.println("Please enter Patient phone number: ");
+           String phonenumber = sc.nextLine();
+           Patient p1=new Patient(name,age,disease,phonenumber);
+
+           System.out.println("Patient added successfully.");
+           System.out.println(p1.getPatientSummary());
+        }catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
         }
     }
 }
