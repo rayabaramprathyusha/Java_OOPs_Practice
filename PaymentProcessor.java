48618e467a4dd091ac9cde34f2d0e74e9143d715
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-07 10:57:39 -0500

    Update PaymentProcessor.java
    do while loop
    polymorphism(Dyanamic Method dispatch)
    Inheritance
    Method overriding

--- PaymentProcessor.java
+++ PaymentProcessor.java
@@ -1,35 +1,58 @@
-class Payment {
-    public void process(double amount) {
-        System.out.println("Generic payment processed: $" + amount);
+import java.util.Scanner;
+
+class Payment{
+    public void process(double amount){
+        System.out.println("Generic Payment processed"+amount);
+    }
+}
+class CreditCard extends Payment{
+    public void process(double amount){
+        System.out.println("Credit Card processed"+amount);
+    }
+}
+class UPI extends Payment{
+    public void process(double amount){
+        System.out.println("UPI Payment processed"+amount);
+    }
+}
+class Crypto extends Payment{
+    public void process(double amount){
+        System.out.println("CryptoPayment processed"+amount);
     }
 }
 
-class CreditCard extends Payment {
-    @Override
-    public void process(double amount) {
-        System.out.println("Credit card payment processed: $" + amount);
-    }
-}
+public class PaymentProcessor{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String choice;
 
-class UPI extends Payment {
-    @Override
-    public void process(double amount) {
-        System.out.println("UPI payment processed successfully.");
-    }
-}
+        System.out.println("Welcome to Payment Processor System");
+        do{
+            System.out.print("Enter payment type(creditcard/upi/crypto): ");
+            String type = sc.nextLine();
 
-class Crypto extends Payment {
-    @Override
-    public void process(double amount) {
-        System.out.println("Crypto payment confirmed on blockchain.");
-    }
-}
+            System.out.print("Enter amount to be paid: ");
+            double amount = sc.nextDouble();
+            sc.nextLine();
 
-public class PaymentProcessor {
-    public static void main(String[] args) {
-        Payment[] payments = { new CreditCard(), new UPI(), new Crypto() };
-        for (Payment p : payments) {
-            p.process(1500);
-        }
+            Payment payment;
+
+            if (type.equalsIgnoreCase("CreditCard")){
+                payment=new CreditCard();
+            }else if (type.equalsIgnoreCase("UPI")){
+                payment=new UPI();
+            }else if (type.equalsIgnoreCase("Crypto")){
+                payment=new Crypto();
+            }else{
+                System.out.println("Invalid Payment. Defaulting to generic payment method");
+                payment=new Payment();
+            }
+            payment.process(amount);
+
+            System.out.println("Do you want to process another payment(yes/no)");
+            choice=sc.nextLine();
+        }while(choice.equalsIgnoreCase("yes"));
+        System.out.println("Thankyou, All payments process successfull");
+        sc.close();
     }
 }
