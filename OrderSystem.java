4ac0002d0c62ad21d82f6fcb270febd22c98295e
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-07 12:03:09 -0500

    Update OrderSystem.java
    interfaces
    Abstract class
    do while
    Inheritance
    polymorphism

--- OrderSystem.java
+++ OrderSystem.java
@@ -1,46 +1,71 @@
-interface PaymentGateway {
+import java.util.Scanner;
+
+interface PaymentGateway{
     void processPayment(double amount);
 }
 
-interface Shipment {
-    void shipOrder(String orderId);
+interface Shipment{
+    void shipOrder(String order_id);
 }
 
-abstract class Order {
-    protected String orderId;
+abstract class Order{
+    protected  String order_id;
     protected double amount;
 
-    Order(String orderId, double amount) {
-        this.orderId = orderId;
+    Order(String order_id,double amount){
+        this.order_id = order_id;
         this.amount = amount;
     }
 
     abstract void generateInvoice();
 }
 
-class AmazonOrder extends Order implements PaymentGateway, Shipment {
-    AmazonOrder(String orderId, double amount) {
-        super(orderId, amount);
-    }
+//concrete class implementing both interfaces
 
-    public void processPayment(double amount) {
-        System.out.println("Amazon Payment processed for $" + amount);
+class AmazonOrder extends Order implements PaymentGateway, Shipment{
+    AmazonOrder(String order_id,double amount){
+        super(order_id,amount);
     }
-
-    public void shipOrder(String orderId) {
-        System.out.println("Order " + orderId + " shipped via Amazon logistics.");
+    public void processPayment(double amount){
+        System.out.println("Amazon Payment processed Successfully: $"+amount);
     }
-
-    void generateInvoice() {
-        System.out.println("Amazon Invoice generated for Order ID: " + orderId);
+    @Override
+    public void shipOrder(String order_id){
+        System.out.println("Amazon Order "+order_id+" shipped successfully:");
+    }
+    void generateInvoice(){
+        System.out.println("Amazon invoice generated :"+order_id);
     }
 }
 
 public class OrderSystem {
     public static void main(String[] args) {
-        AmazonOrder order = new AmazonOrder("ORD12345", 249.99);
-        order.generateInvoice();
-        order.processPayment(order.amount);
-        order.shipOrder(order.orderId);
+        Scanner sc = new Scanner(System.in);
+
+        String choice;
+
+        do{
+            System.out.print("-----------Amazon Order System--------- \n");
+            System.out.print("Enter Order ID: ");
+            String order_id = sc.nextLine();
+
+            System.out.print("Enter Order Amount: ");
+            double amount = sc.nextDouble();
+            sc.nextLine();
+
+            AmazonOrder order = new AmazonOrder(order_id,amount);
+
+            System.out.print("Processing Order");
+            order.generateInvoice();
+            order.processPayment(amount);
+            order.shipOrder(order_id);
+
+            System.out.println("Do you want to process another order(yes/no)");
+            choice = sc.nextLine();
+
+        }while(choice.equalsIgnoreCase("yes"));
+
+        System.out.println("All orders processed successfully");
+        sc.close();
     }
 }
